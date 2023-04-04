788a186d3e778a1833199beccd4402195cb0e1c8
kushagra gupta <kushagraguptastud.02@gmail.com>
2023-04-04 15:38:45 +0530

    finalrelease

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -62,7 +62,7 @@ const Register = () => {
   return (
     <div className="formContainer">
       <div className="formWrapper">
-        <span className="logo">Lama Chat</span>
+        <span className="logo">Snappify</span>
         <span className="title">Register</span>
         <form onSubmit={handleSubmit}>
           <input required type="text" placeholder="display name" />
